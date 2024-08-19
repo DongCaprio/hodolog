c65b956292352827d44eec67972488d289ccb76b
김동진 <dongjin403@naver.com>
2024-08-19 23:50:12 +0900

    인증추가하기(Interceptor 사용)

--- src/main/java/com/hodolog/api/config/WebMvcConfig.java
+++ src/main/java/com/hodolog/api/config/WebMvcConfig.java
@@ -8,14 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class WebMvcConfig
-        implements WebMvcConfigurer {
-/*    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor())
-                .excludePathPatterns("/error", "/favicon.ico");
-    }*/
-
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthResolver());
